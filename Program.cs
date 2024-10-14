47e12e27a72e91b8675b4e79718a7e3518ed5a85
Victor Eduardo Almeida Bispo <v2.abispo@gmail.com>
2024-10-14 16:02:20 -0300

    Desafio concluido com Sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"147852", modelo:"modelo1", imei:"11111", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"369852", modelo:"modelo2", imei:"22222", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
 // TODO: Realizar os testes com as classes Nokia e Iphone
